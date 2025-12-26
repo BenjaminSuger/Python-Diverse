101d64a421dad15afa02912c343e521d94c8465b
sugerbenjamin@prontonmail.com <sugerbenjamin@prontonmail.com>
2025-12-26 21:01:33 +0100

    Module 00 ex02 and ex03

---
+++ Python_Modules/Module00/ft_harvest_total.py
@@ -0,0 +1,7 @@
+def ft_harvest_total() :
+    total = int(input("Day 1 harvest: "))
+    total += int(input("Day 2 harvest: "))
+    total += int(input("Day 3 harvest: "))
+    print(f"Total harvest: {total}")
+
+#ft_harvest_total()
