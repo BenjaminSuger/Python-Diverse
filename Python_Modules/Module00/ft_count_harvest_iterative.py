c5d4366a039c8969a86df877a9bee559391fe17a
sugerbenjamin@prontonmail.com <sugerbenjamin@protonmail.com>
2026-01-21 22:25:40 +0100

    add ft_count_harvest_iterative.py

---
+++ Python_Modules/Module00/ft_count_harvest_iterative.py
@@ -0,0 +1,8 @@
+def ft_count_harvest_iterative():
+    print("Days until harvest:", end=" ")#change to not put \n
+    value = int(input())
+    for i in range(0, value + 1):
+        print(f"Day {i}")
+    print("Harvest time!")
+
+ft_count_harvest_iterative()
