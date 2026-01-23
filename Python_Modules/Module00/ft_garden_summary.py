0903038f6b3eaf3f2f1e6dea635feb657f3c7ea5
sugerbenjamin@prontonmail.com <sugerbenjamin@protonmail.com>
2026-01-23 21:08:55 +0100

    finished Module00

---
+++ Python_Modules/Module00/ft_garden_summary.py
@@ -0,0 +1,9 @@
+def ft_garden_summary():
+    print("Enter garden name:", end=" ")
+    name = input()
+    print("Enter number of plant:", end=" ")
+    value = input()
+    print(f"Garden: {name}\nPlants: {value}\nStatus: Growing well!")
+
+
+ft_garden_summary()
---
+++ Python_Modules/Module00/ft_seed_inventory.py
@@ -0,0 +1,16 @@
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    unit = unit.lower() # pas sur de ca
+    seed_type = seed_type.lower()
+    match unit:
+        case "packets":
+            print(f"{seed_type} seeds: {quantity} availaable")
+        case "grams":
+            print(f"{seed_type} seeds: {quantity} grams total")
+        case "area":
+            print(f"{seed_type} seeds: covers {quantity} square meters")
+        case _:
+            print("Unknown unit type")
+
+ft_seed_inventory("tomato", 15, "packets")
+ft_seed_inventory("carrot", 8, "grams")
+ft_seed_inventory("lettuce", 12, "area")
