a2ad0ffc0212350ba081596c24217833a1580e8b
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-01 11:30:44 +0200

    Piscine Python day00 ex07

--- Python_Piscine/day00/ex07/sos.py
+++ Python_Piscine/day00/ex07/sos.py
@@ -1,7 +1,29 @@
 from sys import argv
 
-NESTED_MORSE = { " ": "/ ", "A": ".- "}
+
+def convert_morse(sentence: str) -> str:
+    NESTED_MORSE = {" ": "/ ", "A": ".- ", "B": " -... ", "C": "-.-. ",
+                    "E": ". ", "F": "..-. ", "G": "--. ", "H": ".... ",
+                    "I": ".. ", "J": ".--- ", "K": "-.- ", "L": ".-.. ",
+                    "M": "-- ", "N": "-. ", "O": "--- ", "P": ".--. ",
+                    "Q": "--.- ", "R": ".-. ", "S": "... ", "T": "- ",
+                    "U": "..- ", "V": "...- ", "W": ".-- ", "X": "-..- ",
+                    "Y": "-.-- ", "Z": "--.. ", "1": ".---- ", "2": "..--- ",
+                    "3": "...-- ", "4": "....- ", "5": "..... ", "6": "-.... ",
+                    "7": "--... ", "8": "---.. ", "9": "----. ", "0": "----- "}
+    result = []
+    for i in sentence:
+        result.append(NESTED_MORSE[i.upper()])
+    return ''.join(result)
 
 
 if __name__ == "__main__":
-
+    try:
+        if len(argv) != 2:
+            raise AssertionError("the arguments are bad")
+        for i in argv[1]:
+            if not (i.isalnum() or i == ' '):
+                raise AssertionError("the arguments are bad")
+        print(convert_morse(argv[1]))
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
