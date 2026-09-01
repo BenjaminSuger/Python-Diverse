4060916e1cfc8876fd71c9fca69673342440380f
sugerbenjamin@prontonmail.com <sugerbenjamin@protonmail.com>
2026-09-01 21:47:29 +0200

    Loading.py modifiy with the beginning of calculations

--- Python_Piscine/day00/ex08/Loading.py
+++ Python_Piscine/day00/ex08/Loading.py
@@ -6,10 +6,14 @@ def ft_tqdm(lst: range) -> None:
 
 
 if __name__ == "__main__":
-    columns = os.get_terminal_size().columns - 6 # minus 6 for the first char
-    value = 0
     for percentage in range(0, 101):
-        print(f"\r{percentage:>3}% |" + (columns * "a"), end='')
+        left = f"\r{percentage:>3}%|"
+        right =  "| 333/333 [00:01<00:00, 177.76it/s]"
+        #f"| {n}/{total} [{elapsed}<{remaining}, {rate}it/s]"
+        columns = os.get_terminal_size().columns - len(left) - len(right)
+        #percentage =  
+        middle = columns * '██'  #int(columns * percentage / 100) * '██'
+        print("\r" + left + middle + right, end='', flush=True)
         time.sleep(0.05)
     
 
