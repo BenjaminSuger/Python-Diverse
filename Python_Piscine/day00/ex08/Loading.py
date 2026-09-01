650495ca68828789aa6425188a3de3bd58e1db92
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-01 17:31:48 +0200

    Python Piscine day00 ex08 start testing

--- Python_Piscine/day00/ex08/Loading.py
+++ Python_Piscine/day00/ex08/Loading.py
@@ -1,4 +1,16 @@
-
+import os # temporaire 
+import time
 
 def ft_tqdm(lst: range) -> None:
     pass
+
+
+if __name__ == "__main__":
+    columns = os.get_terminal_size().columns - 6 # minus 6 for the first char
+    value = 0
+    for percentage in range(0, 101):
+        print(f"\r{percentage:>3}% |" + (columns * "a"), end='')
+        time.sleep(0.05)
+    
+
+    
