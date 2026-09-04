da1b25447b1118ffad1afccf56291dde81d0e65d
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-04 18:03:09 +0200

    Piscine Python day01 start ex00

---
+++ Python_Piscine/day01/ex00/tester.py
@@ -0,0 +1,11 @@
+from give_bmi import give_bmi, apply_limit
+
+height = [2.71, 1.15]
+weight = [165.3, 38.4]
+
+try:
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+except ValueError as e:
+    print(f"{type(e).__name__} : {e}")
