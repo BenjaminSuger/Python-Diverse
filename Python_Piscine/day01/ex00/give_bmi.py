db91305cf757a8480bc23995c63031a42190f99d
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-04 19:55:19 +0200

    Python Piscine day01 ex00

--- Python_Piscine/day01/ex00/give_bmi.py
+++ Python_Piscine/day01/ex00/give_bmi.py
@@ -1,11 +1,30 @@
 from numpy import multiply
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
 
+def check_input(input: list[int | float]) -> bool:
+    """check the input needs to be a list and int or float"""
+    if not isinstance(input, list):
+        return False
+    breakpoint()
+    for i in input:
+        if not isinstance(i, float) and not isinstance(i, int):
+            return False
+    return True
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    """give the bmi with multiplication from numpy"""
+    #check if the input is good
+    
 
 
 
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """for each element in bmi check the limit"""
+    #il faut bien maitriser les histoire de comparaison int avec float pour etre sur comment ca marche (parce que 2 = 1.99999) donc la il y a un gros piege 
     pass
+
+if __name__ == "__main__":
+    print(check_input([3,4,5,6.0, 'a']))
