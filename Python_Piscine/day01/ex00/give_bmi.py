7493ffe03671ea4157ace8d03c71a19e3edecd3a
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-14 17:34:36 +0200

    piscine python day01 ex00 correction => best management of errors messages

--- Python_Piscine/day01/ex00/give_bmi.py
+++ Python_Piscine/day01/ex00/give_bmi.py
@@ -1,35 +1,35 @@
 from numpy import divide, power
 
 
-def check_input(input: list[int | float]) -> bool:
+def check_input(input: list[int | float]) -> None:
     """check the input needs to be a list and int or float"""
     if not isinstance(input, list):
-        return False
+        raise TypeError("Wrong Input: is not a list")
     if not input:
-        return False
+        raise ValueError("Wrong Input: empty list")
     for i in input:
         if not isinstance(i, (int, float)):
-            return False
+            raise TypeError("Wrong Input: is not composed of int or float")
         if i <= 0:
-            return False
-    return True
+            raise ValueError("Wrong Input: Value cannot be 0 or negative")
 
 
 def give_bmi(height: list[int | float],
              weight: list[int | float]) -> list[int | float]:
     """give the bmi from 2 lists with numpy divide and power
     BMI formula weight / height^2 """
-    if not check_input(height) or not check_input(weight):
-        raise AssertionError("give_bmi ; Input not valid")
+    check_input(height)
+    check_input(weight)
     if len(height) != len(weight):
-        raise AssertionError("give_bmi ; Input not valid")
+        raise AssertionError("give_bmi ; lists are not the same size")
     return divide(weight, power(height, 2)).tolist()
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """for each element in bmi check the limit"""
-    if not check_input(bmi) or not isinstance(limit, int):
-        raise AssertionError("Apply_limit ; input not valid")
+    check_input(bmi)
+    if not isinstance(limit, int):
+        raise TypeError("Apply_limit ; limit should be int")
     return [True if x > limit else False for x in bmi]
 
 
@@ -39,5 +39,5 @@ if __name__ == "__main__":
         result_apply = apply_limit(result, 15)
         print(f"Give BMI ; {result}")
         print(f"Apply limit; {result_apply}")
-    except AssertionError as e:
+    except (TypeError, AssertionError, ValueError) as e:
         print(f"{type(e).__name__} {e}")
--- Python_Piscine/day01/ex00/tester.py
+++ Python_Piscine/day01/ex00/tester.py
@@ -1,11 +1,35 @@
 from give_bmi import give_bmi, apply_limit
 
-height = [2.71, 1.15]
-weight = [165.3, 38.4]
 
-try:
-    bmi = give_bmi(height, weight)
-    print(bmi, type(bmi))
-    print(apply_limit(bmi, 26))
-except AssertionError as e:
-    print(f"AssertionError : {e}")
+def main() -> None:
+    """main tester"""
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]
+    try:
+        bmi = give_bmi(height, weight)
+        print(bmi, type(bmi))
+        print(apply_limit(bmi, 26))
+    except (TypeError, AssertionError, ValueError) as e:
+        print(f"AssertionError : {e}")
+    try:
+        height = (2.71, 1.15)
+        weight = [165.3, 38.4]
+        bmi = give_bmi(height, weight)
+    except (TypeError, AssertionError, ValueError) as e:
+        print(f"AssertionError : {e}")
+    try:
+        height = [2.71, 1.15]
+        weight = [165.3, 'a']
+        bmi = give_bmi(height, weight)
+    except (TypeError, AssertionError, ValueError) as e:
+        print(f"AssertionError : {e}")
+    try:
+        height = [2.71, 1.15]
+        weight = [165.3, -42.4]
+        bmi = give_bmi(height, weight)
+    except (TypeError, AssertionError, ValueError) as e:
+        print(f"AssertionError : {e}")
+
+
+if __name__ == "__main__":
+    main()
