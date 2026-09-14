f815f21ea76114dcb1c9248c1512c442d8dd97f1
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-14 14:10:42 +0200

    format_ft_time.py small error correction

--- Python_Piscine/day00/ex01/format_ft_time.py
+++ Python_Piscine/day00/ex01/format_ft_time.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 time = datetime.now()
-print(f"Seconds since January 1, 1970:{time.timestamp(): .0f} or{time.timestamp(): .2e} in scientific notation")
+print(f"Seconds since January 1, 1970:{time.timestamp(): ,.4f} or{time.timestamp(): .2e} in scientific notation")
 print(time.strftime("%b"), time.day, time.year)
