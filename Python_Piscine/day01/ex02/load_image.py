6d2d1111a33a2598b36f70d98b0fac26c3db22c9
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-15 09:53:38 +0200

    piscine python day01 ex02 update

--- Python_Piscine/day01/ex02/load_image.py
+++ Python_Piscine/day01/ex02/load_image.py
@@ -1,10 +1,19 @@
 import numpy
 from PIL import Image
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #la j'aurais peut etre pas besoin pour cette exo mais pour les prochains
 
-def ft_load(path: str) -> array #je peux changer l'output
-    pass
+#faudra bien tester aussi une image "vide"
+
+def ft_load(path: str) -> array: #je peux changer l'output
+    """Load JPG or JPEG and print information about the img"""
+    img = Image.open(path)
+    print(img.load()) 
+
 
 
 if __name__ == "__main__":
-    pass
+    try:
+        print(ft_load("./images/animal.jpeg"))
+        print(ft_load("tester.py"))
+    except Exception as e:
+        print(f"{type(e).__name__} : {e}")
--- Python_Piscine/day01/ex02/tester.py
+++ Python_Piscine/day01/ex02/tester.py
@@ -1,10 +1,20 @@
 from load_image import ft_load
 
+'''
+pense a mettre plein de test de chose qui marche pas;
+pas de permission (bon juste a la correction)
+fichier non existant
+pas de JPEG or JPG mais qui est une image
+pas un fichier image du tout genre un le fichier.py
+un JPEG ou JPG "vide" => je sais pas encore le comportement
+'''
 
 def main() -> None:
     """main tester"""
-    print(ft_load("landscape.jpg"))
-
+    try:
+        print(ft_load("./images/landscape.jpg"))
+    except Exception as e:
+        print(f"{(type(e).__name__} : {e}")
 
 if __name__ == "__main__":
     main()
