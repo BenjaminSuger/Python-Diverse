77b914a8e798de72c4f4c1be3f034a59bed51ac1
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-16 15:44:44 +0200

    Python Piscine day01 ex02 done

--- Python_Piscine/day01/ex02/load_image.py
+++ Python_Piscine/day01/ex02/load_image.py
@@ -1,19 +1,19 @@
-import numpy
+from numpy import asarray
 from PIL import Image
-import matplotlib.pyplot as plt #la j'aurais peut etre pas besoin pour cette exo mais pour les prochains
 
-#faudra bien tester aussi une image "vide"
 
-def ft_load(path: str) -> array: #je peux changer l'output
+def ft_load(path: str) -> list:
     """Load JPG or JPEG and print information about the img"""
     img = Image.open(path)
-    print(img.load()) 
-
+    if img.format != "JPEG":
+        raise TypeError("Image is not a JPEG or JPG")
+    result = asarray(img)
+    print(f"The shape of image is : {result.shape}")
+    return result
 
 
 if __name__ == "__main__":
     try:
-        print(ft_load("./images/animal.jpeg"))
-        print(ft_load("tester.py"))
+        print(ft_load("./images/landscape.jpg"))
     except Exception as e:
         print(f"{type(e).__name__} : {e}")
--- Python_Piscine/day01/ex02/tester.py
+++ Python_Piscine/day01/ex02/tester.py
@@ -1,20 +1,22 @@
 from load_image import ft_load
 
-'''
-pense a mettre plein de test de chose qui marche pas;
-pas de permission (bon juste a la correction)
-fichier non existant
-pas de JPEG or JPG mais qui est une image
-pas un fichier image du tout genre un le fichier.py
-un JPEG ou JPG "vide" => je sais pas encore le comportement
-'''
 
 def main() -> None:
     """main tester"""
     try:
         print(ft_load("./images/landscape.jpg"))
     except Exception as e:
-        print(f"{(type(e).__name__} : {e}")
+        print(f"{type(e).__name__} : {e}")
+    print("\n========OTHER TEST========")
+    try:
+        print(ft_load("./images/potit_chat_fait_dodo.png"))
+    except Exception as e:
+        print(f"{type(e).__name__} : {e}")
+    try:
+        print(ft_load("does_not_exist"))
+    except Exception as e:
+        print(f"{type(e).__name__} : {e}")
+
 
 if __name__ == "__main__":
     main()
