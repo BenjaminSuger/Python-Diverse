5e9204cb15da8826b091fac38b64156618dc5b46
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-18 11:46:48 +0200

    Python Piscine add image animal and setup zoom.py

--- Python_Piscine/day01/ex03/tester.py
+++ Python_Piscine/day01/ex03/tester.py
@@ -1,5 +1,5 @@
 from load_image import ft_load
-
+#je sais pas si j'aurais tant besoin de ce fichier peut etre laisser load_image.py
 
 def main() -> None:
     """main tester"""
--- Python_Piscine/day01/ex03/zoom.py
+++ Python_Piscine/day01/ex03/zoom.py
@@ -1,4 +1,16 @@
+from load_image import ft_load
 import matplotlib.pyplot as plt #temporaire parce que pas legit
 
 
+def ft_zoom(path: str, zoom: int ) -> list:
+    # a mon avis des securite sur le zoom
+    # mais je sasi pas comment faire le zoom
+    arr = ft_load(path)
+    print(arr)
 
+
+if __name__ == "__main__":
+    try:
+        ft_zoom("./images/animal.jpeg")
+    except Exception as e:
+        print(f"{type(e).__name__} : {e}")
