880cf9a1b0bfda36149947438aede04f45c45aeb
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-23 16:35:20 +0200

    Python Piscine : zoom.py setup but needs output format

--- Python_Piscine/day01/ex03/zoom.py
+++ Python_Piscine/day01/ex03/zoom.py
@@ -2,42 +2,31 @@ from load_image import ft_load
 import matplotlib.pyplot as plt #temporaire parce que pas legit
 #je vais peut etre importe scipy pour le zoom mais apres je comprend pas l'output
 
-def ft_zoom(path: str, zoom: int ) -> list:
-    # a mon avis des securite sur le zoom
-    # mais je sasi pas comment faire le zoom
+def ft_zoom(path: str) -> list:
+    """zoom on the image with a calculation from the
+    original dimension (zoom == 2)"""
     arr = ft_load(path)
     print(arr)
 
-    arr = arr[300:600, 300:600, 0] #la j'ai fais le slicing pour le moment a la main et ensuite faudra calculer (voir plus bas)
+    height, length = arr.shape[0], arr.shape[1]
+    nouvelle_h = height // 2
+    nouvelle_l = length // 2
+    centre_y = height // 2
+    centre_x = length // 2
+    debut_y = centre_y - nouvelle_h // 2
+    debut_x = centre_x - nouvelle_l // 2
 
-    
-    plt.imshow(arr, cmap='gray')
+#reverifier si le 0 necessaire
+    zoom_arr = arr[debut_y:debut_y + nouvelle_h, debut_x:debut_x + nouvelle_l, 0]
+    plt.imshow(zoom_arr, cmap='gray')
     plt.show()
-
-    print(f"New shape after slicing: ")
-    print(arr)
-
+    #ici probleme car je dois avoir un qui affiche les channels (troisieme truc)
+    print(f"New shape after slicing: {zoom_arr.shape} or {zoom_arr.shape}")
 
 
 
 if __name__ == "__main__":
     try:
-        ft_zoom("./images/animal.jpeg", 2)
+        ft_zoom("./images/animal.jpeg")
     except Exception as e:
         print(f"{type(e).__name__} : {e}")
-
-
-'''
-hauteur, largeur = arr.shape[0], arr.shape[1]
-
-nouvelle_h = hauteur // zoom
-nouvelle_l = largeur // zoom
-
-centre_y = hauteur // 2
-centre_x = largeur // 2
-
-debut_y = centre_y - nouvelle_h // 2
-debut_x = centre_x - nouvelle_l // 2
-
-zoom_arr = arr[debut_y:debut_y + nouvelle_h, debut_x:debut_x + nouvelle_l, 0]
-'''
