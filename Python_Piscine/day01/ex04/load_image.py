e16f5050eadf5b6ad3fd73c4ccea7c9a1032cc85
bsuger <106188326+BenjaminSuger@users.noreply.github.com>
2026-09-25 10:01:15 +0200

    Python Piscine day01 prepare ex04 and ex05 files

---
+++ Python_Piscine/day01/ex04/load_image.py
@@ -0,0 +1,19 @@
+from numpy import asarray
+from PIL import Image
+
+
+def ft_load(path: str) -> list:
+    """Load JPG or JPEG and print information about the img"""
+    img = Image.open(path)
+    if img.format != "JPEG":
+        raise TypeError("Image is not a JPEG or JPG")
+    result = asarray(img)
+    print(f"The shape of image is : {result.shape}")
+    return result
+
+
+if __name__ == "__main__":
+    try:
+        print(ft_load("./images/landscape.jpg"))
+    except Exception as e:
+        print(f"{type(e).__name__} : {e}")
